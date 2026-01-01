3d7b0a2720dfe921c46988cf02db380a49d3c9d2
Andre Winkler <gluehloch@googlemail.com>
2026-01-01 20:55:29 +0100

    Make it runnable...

--- src/main/java/de/awtools/lab/oauth2/ApplicationConfiguration.java
+++ src/main/java/de/awtools/lab/oauth2/ApplicationConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 @Configuration
@@ -14,10 +15,22 @@ public class ApplicationConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
             .authorizeHttpRequests((authz) -> authz
                 .requestMatchers("/", "/error", "/webjars/**", "/index.html",
-                    "/login/oauth2/code/github").permitAll()
+                    "/login/oauth2/code/github", "/logout").permitAll()
                 .anyRequest().authenticated())
+                /* 
+                .logout(l ->
+                    l.invalidateHttpSession(false)
+                    .logoutSuccessUrl("/").permitAll())
+                    */
+                    /*
+logout.deleteCookies("remove")
+.invalidateHttpSession(false)
+.logoutUrl("/custom-logout")
+.logoutSuccessUrl("/logout-success")
+                    */
             .oauth2Client(Customizer.withDefaults())
             .oauth2Login(Customizer.withDefaults());
         return http.build();
