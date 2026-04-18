4e198ab78452ca1553b94b7f87b4a931198fde7a
Andre Winkler <gluehloch@googlemail.com>
2026-04-18 17:07:59 +0200

    WIP: Logout does not work?

--- src/main/java/de/awtools/lab/oauth2/ApplicationConfiguration.java
+++ src/main/java/de/awtools/lab/oauth2/ApplicationConfiguration.java
@@ -8,8 +8,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter.Directive;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 
 @EnableWebSecurity
@@ -21,21 +24,22 @@ public class ApplicationConfiguration {
         HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
         requestCache.setMatchingRequestParameterName(null);
 
+        HeaderWriterLogoutHandler clearSiteData = new HeaderWriterLogoutHandler(new ClearSiteDataHeaderWriter(Directive.ALL));
+
         http.requestCache(cache -> cache.requestCache(requestCache))
             .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
             .authorizeHttpRequests((authz) -> authz
                 .requestMatchers( "/error", "/favicon.ico", "/css/**", "/script/**", "/index.html").permitAll()
                 .anyRequest().authenticated())
             .logout(l -> l
+            	.logoutUrl("/logout")
                 .logoutSuccessUrl("/")
                 .deleteCookies("X-XSRF-TOKEN")
-                .invalidateHttpSession(false)
-                .permitAll())
-            .oauth2Client(Customizer.withDefaults())
-            .oauth2Login(login -> login.defaultSuccessUrl("/", true))
-            .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
-        http.addFilterAfter(new CsrfCookieFilter(), CsrfFilter.class);
-        return http.build();
-    }
+                .invalidateHttpSession(false).addLogoutHandler(clearSiteData).permitAll())
+				.oauth2Client(Customizer.withDefaults()).oauth2Login(login -> login.defaultSuccessUrl("/", true))
+				.exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
+		http.addFilterAfter(new CsrfCookieFilter(), CsrfFilter.class);
+		return http.build();
+	}
 
 }
